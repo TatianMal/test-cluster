2499e1a8259bc36dfefe3acc1ef0be31cb32c81a
Tatiana Maltseva <tatmal.educ@gmail.com>
2021-08-25 14:08:47 +0300

    fixed babel config file extension, removed some dependencies

--- babel.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-  };
-  /* TODO: разобраться, почему работает только cjs
-  https://stackoverflow.com/questions/61146112/error-while-loading-config-you-appear-to-be-using-a-native-ecmascript-module-c
-  https://jestjs.io/ru/docs/configuration
-  */
